ffd54ddb2efcaf90727e5c577904cb4a2d132e05
hestarium <hestarium@hotmail.com>
2019-05-22 00:12:35 +0900

    [KSJ] Make Sun Make Sun
    나중에 해야지 한글로 써야지 으아아아아

---
+++ src/main/java/Sun.java
@@ -0,0 +1,51 @@
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Sun implements Mode {
+
+    private RealTime realTime;
+    private Calendar[] sun;
+    private String[] city;
+    private int currCity;
+    private boolean currMode; // true: rise, false: set
+
+    public Sun(){
+        this.realTime = null;
+        this.sun = new Calendar[2];
+        for(Calendar cal : this.sun){
+            cal = Calendar.getInstance();
+            cal.clear();
+        }
+
+        this.city = TimeZone.getAvailableIDs();
+        this.currCity = 0;
+        this.currMode = true;
+    }
+
+    public void setRealTime(RealTime realTime){
+        this.realTime = realTime;
+
+    }
+
+    public void realTimeTaskSun(){
+
+    }
+
+    public void requestSetRise(){
+        this.currMode = !this.currMode;
+    }
+
+    public void requestNextCity(){
+
+    }
+
+    public void requestPrevCity(){
+
+    }
+
+    private Calendar[] calculateSun(){
+
+    }
+
+
+}
